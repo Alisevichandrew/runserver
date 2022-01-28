7b04d5a7083670cc7805c008a7017ac6fbdf384f
Alisevichandrew <dopysk@yandex.ru>
2022-01-28 15:09:36 +0300

    first commit

---
+++ base/api/views.py
@@ -0,0 +1,14 @@
+from multiprocessing.spawn import import_main_path
+from django.http import JsonResponse
+from  rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/token',
+        '/api/token/refresh',
+    ]
+
+    return Response(routes)
+
